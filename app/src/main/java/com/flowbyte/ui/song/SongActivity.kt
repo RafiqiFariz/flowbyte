926e12aef991f507fb50e8ed992cb566b62abbc8
FarizRafiqi <auliaelihza@gmail.com>
2024-03-25 08:41:14 +0700

    feat: make activity_song.xml, change colorPrimary in themes.xml, add custom font

---
+++ app/src/main/java/com/flowbyte/ui/song/SongActivity.kt
@@ -0,0 +1,12 @@
+package com.flowbyte.ui.song
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.flowbyte.R
+
+class SongActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_song)
+    }
+}
