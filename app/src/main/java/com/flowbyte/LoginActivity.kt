8d06929faeeb072cf2fe334885a830d14298f1a1
Rana E. Fathin <ranaekakurniafathin@gmail.com>
2024-04-02 10:44:18 +0700

    feat: add intent to home fragment from login and register activity

--- app/src/main/java/com/flowbyte/LoginActivity.kt
+++ app/src/main/java/com/flowbyte/LoginActivity.kt
@@ -3,7 +3,9 @@ package com.flowbyte
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import com.flowbyte.ui.home.HomeFragment
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +15,12 @@ class LoginActivity : AppCompatActivity() {
         val textViewSignUp = findViewById<TextView>(R.id.register)
         textViewSignUp.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
+        val loginBtn = findViewById<Button>(R.id.buttonLogin)
+        loginBtn.setOnClickListener{
+            val intent = Intent(this, HomeFragment::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/flowbyte/SignUpActivity.kt
+++ app/src/main/java/com/flowbyte/SignUpActivity.kt
@@ -3,7 +3,9 @@ package com.flowbyte
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import com.flowbyte.ui.home.HomeFragment
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +15,12 @@ class SignUpActivity : AppCompatActivity() {
         val textViewSignUp = findViewById<TextView>(R.id.login)
         textViewSignUp.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        val registerBtn = findViewById<Button>(R.id.buttonRegister)
+        registerBtn.setOnClickListener{
+            val intent = Intent(this, HomeFragment::class.java)
             startActivity(intent)
         }
     }
