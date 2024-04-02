1437d2252219fa2c59201fcd9e8ae140994484c7
Pandu Utomo <panxzzlyn@gmail.com>
2024-04-02 13:12:45 +0700

    feat: update intent

--- app/src/main/java/com/flowbyte/SplashScreenActivity.kt
+++ app/src/main/java/com/flowbyte/SplashScreenActivity.kt
@@ -5,13 +5,13 @@ import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 
-class Splashscreen : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
 
         Handler().postDelayed({
-            startActivity(Intent(this@Splashscreen, gettingstarted::class.java))
+            startActivity(Intent(this@SplashScreenActivity, GettingStartedActivity::class.java))
             finish()
         }, 7000)
 
