ff56c079f9965bfa0c16de1be23f80e2c331b5c5
Pandu Utomo <panxzzlyn@gmail.com>
2024-04-02 10:41:19 +0700

    feat: recylerView For Explore

---
+++ app/src/main/java/com/flowbyte/ListGenre.kt
@@ -0,0 +1,3 @@
+package com.flowbyte
+
+data class ListGenre(var title: String, var image: Int)
---
+++ app/src/main/java/com/flowbyte/RecylerViewListGenreAdapter.kt
@@ -0,0 +1,43 @@
+package com.flowbyte
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.cardview.widget.CardView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerViewListGenreAdapter(private val getSpecificFragment: () -> Fragment, private val ListGenre: List<ListGenre>) : RecyclerView.Adapter<RecyclerViewListGenreAdapter.MyViewHolder>() {
+    // Adapter implementation here
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): RecyclerViewListGenreAdapter.MyViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_card_components, parent, false)
+
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerViewListGenreAdapter.MyViewHolder, position: Int) {
+        holder.GenreListTitle.text = ListGenre[position].title
+        holder.GenreListImg.setImageResource(ListGenre[position].image)
+
+        holder.CardView.setOnClickListener {
+            Toast.makeText(holder.itemView.context, ListGenre[position].title, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return ListGenre.size
+    }
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val GenreListTitle: TextView = itemView.findViewById(R.id.cardText)
+        val GenreListImg: ImageView = itemView.findViewById(R.id.imageCard)
+        val CardView: CardView = itemView.findViewById(R.id.cardView)
+    }
+
+}
