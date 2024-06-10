8faf2b2984ee902f253db0d85524c6413274ec3a
RafiqiFariz <elihza030107@gmail.com>
2024-06-11 01:13:14 +0700

    feat: memfungsikan music player pada activity_song.xml, mengganti warna teks getting started menjadi putih dan menyoroti beberapa kata

--- app/src/main/java/com/flowbyte/GettingStartedActivity.kt
+++ app/src/main/java/com/flowbyte/GettingStartedActivity.kt
@@ -1,18 +1,56 @@
 package com.flowbyte
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import android.widget.Button
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 
 class GettingStartedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gettingstarted)
         supportActionBar?.hide()
+
+        val text =
+            "Explore unlimited music with our app, accessing your favorite songs and playlists"
+
+        // Define words to highlight
+        val highlightWords = listOf("unlimited music", "favorite songs")
+
+        // Create a SpannableString from the text
+        val spannableString = SpannableString(text)
+
+        // Get the color from resources
+        val highlightColor = ContextCompat.getColor(this, R.color.light_sea_green)
+
+        // Apply highlight to each word
+        for (word in highlightWords) {
+            val startIndex = text.indexOf(word)
+            if (startIndex >= 0) {
+                val endIndex = startIndex + word.length
+                spannableString.setSpan(
+                    ForegroundColorSpan(highlightColor),
+                    startIndex,
+                    endIndex,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+            }
+        }
+
+        // Set the spannable string to the TextView
+        val desc = findViewById<TextView>(R.id.desc)
+        desc.text = spannableString
+
         val btnGettingStart = findViewById<Button>(R.id.buttonGettingStarted)
         btnGettingStart.setOnClickListener {
             val intentLogin = Intent(this, LoginActivity::class.java)
+//            val intentLogin = Intent(this, MainActivity::class.java) // utk bypass
             startActivity(intentLogin)
         }
     }
--- app/src/main/java/com/flowbyte/ui/song/SongActivity.kt
+++ app/src/main/java/com/flowbyte/ui/song/SongActivity.kt
@@ -1,13 +1,66 @@
 package com.flowbyte.ui.song
 
+import android.content.ContentResolver
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.OptIn
+import androidx.media3.common.AudioAttributes
+import androidx.media3.common.MediaItem
+import androidx.media3.common.PlaybackException
+import androidx.media3.common.Player
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.exoplayer.ExoPlayer
 import com.flowbyte.R
+import com.flowbyte.databinding.ActivitySongBinding
 
 class SongActivity : AppCompatActivity() {
+    private var _binding: ActivitySongBinding? = null
+    private lateinit var _player: ExoPlayer
+
+    @OptIn(UnstableApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_song)
-        supportActionBar?.hide()
+        _binding = ActivitySongBinding.inflate(layoutInflater)
+        setContentView(_binding?.root)
+
+        _player = ExoPlayer.Builder(this)
+            .setAudioAttributes(AudioAttributes.DEFAULT, true)
+            .build()
+        _binding?.playerControlView?.player = _player
+
+//        val mediaItem = MediaItem.fromUri("https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/936/royalty-1619082033-7RC2AlRdd1.mp3")
+        val dummyMusic = Uri.Builder()
+            .scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
+            .authority(packageName)
+            .path(R.raw.royalty.toString())
+            .build()
+        val mediaItem = MediaItem.fromUri(dummyMusic)
+        _player.setMediaItem(mediaItem)
+        _player.prepare()
+        _player.play()
+
+        _player.addListener(object : Player.Listener {
+            override fun onPlayerError(error: PlaybackException) {
+                Toast.makeText(applicationContext, "Error playing media", Toast.LENGTH_SHORT).show()
+                super.onPlayerError(error)
+            }
+        })
+    }
+
+    override fun onStart() {
+        super.onStart()
+        _player.playWhenReady = true
+    }
+
+    override fun onStop() {
+        super.onStop()
+        _player.playWhenReady = false
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _player.release()
     }
 }
