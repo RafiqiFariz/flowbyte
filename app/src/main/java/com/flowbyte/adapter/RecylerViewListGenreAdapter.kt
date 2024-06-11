251dfb3f7850d5cc8e8de1937f55017eb7587c2d
Pandu Utomo <panxzzlyn@gmail.com>
2024-06-11 22:39:01 +0700

    feat: get genre

--- app/src/main/java/com/flowbyte/adapter/RecylerViewListGenreAdapter.kt
+++ app/src/main/java/com/flowbyte/adapter/RecylerViewListGenreAdapter.kt
@@ -9,37 +9,37 @@ import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.flowbyte.R
 import com.flowbyte.data.ListGenre
 
-class RecyclerViewListGenreAdapter(private val getSpecificFragment: () -> Fragment, private val ListGenre: List<ListGenre>) : RecyclerView.Adapter<RecyclerViewListGenreAdapter.MyViewHolder>() {
-    // Adapter implementation here
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): MyViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_card_components, parent, false)
+class RecyclerViewListGenreAdapter(private val getSpecificFragment: () -> Fragment, private val listGenre: List<ListGenre>) : RecyclerView.Adapter<RecyclerViewListGenreAdapter.MyViewHolder>() {
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_card_components, parent, false)
         return MyViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.GenreListTitle.text = ListGenre[position].title
-        holder.GenreListImg.setImageResource(ListGenre[position].image)
+        val genre = listGenre[position]
+        holder.genreListTitle.text = genre.name
+        Glide.with(holder.itemView.context)
+            .load(genre.picture)
+            .placeholder(R.drawable.placeholder) // Add a placeholder image in case the image is loading or fails to load
+            .into(holder.genreListImg)
 
-        holder.CardView.setOnClickListener {
-            Toast.makeText(holder.itemView.context, ListGenre[position].title, Toast.LENGTH_LONG).show()
+        holder.cardView.setOnClickListener {
+            Toast.makeText(holder.itemView.context, genre.name, Toast.LENGTH_LONG).show()
         }
     }
 
     override fun getItemCount(): Int {
-        return ListGenre.size
+        return listGenre.size
     }
 
     class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val GenreListTitle: TextView = itemView.findViewById(R.id.cardText)
-        val GenreListImg: ImageView = itemView.findViewById(R.id.imageCard)
-        val CardView: CardView = itemView.findViewById(R.id.cardView)
+        val genreListTitle: TextView = itemView.findViewById(R.id.cardText)
+        val genreListImg: ImageView = itemView.findViewById(R.id.imageCard)
+        val cardView: CardView = itemView.findViewById(R.id.cardView)
     }
-
 }
--- app/src/main/java/com/flowbyte/data/ListGenre.kt
+++ app/src/main/java/com/flowbyte/data/ListGenre.kt
@@ -1,3 +1,10 @@
 package com.flowbyte.data
 
-data class ListGenre(var title: String, var image: Int)
+data class GenreResponse(
+    val data: List<ListGenre>
+)
+data class ListGenre(
+    val id: Int,
+    val name: String,
+    val picture: String
+)
