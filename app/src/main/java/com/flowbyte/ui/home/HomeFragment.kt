97706ea81a9b2f4a73c0bd61c45e37a10c3def02
salazars01 <90942445+salazars01@users.noreply.github.com>
2024-06-16 14:22:31 +0700

    update homepage remake to recycleview

--- app/src/main/java/com/flowbyte/ui/home/HomeFragment.kt
+++ app/src/main/java/com/flowbyte/ui/home/HomeFragment.kt
@@ -1,50 +1,57 @@
 package com.flowbyte.ui.home
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import com.flowbyte.R
+import com.flowbyte.adapter.RecyclerViewAlbumAdapter
+import com.flowbyte.data.SongAlbumItem
 import com.flowbyte.databinding.FragmentHomeBinding
-import com.flowbyte.ui.song.SongActivity
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
+    private lateinit var adapter: RecyclerViewAlbumAdapter
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
         (requireActivity() as AppCompatActivity?)?.supportActionBar?.hide()
-        val homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
+        val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-//        val textView: TextView = binding.textHome
         homeViewModel.text.observe(viewLifecycleOwner) {
-//            textView.text = it
+            // Update UI if needed
         }
-        binding.cardView1.setOnClickListener {
-            val intent = Intent(this.requireContext(), SongActivity::class.java)
-            startActivity(intent)
-        }
+
+        val listAlbum = listOf(
+            SongAlbumItem(R.drawable.genre, "Album 1"),
+            SongAlbumItem(R.drawable.genre, "Album 2"),
+            SongAlbumItem(R.drawable.genre, "Album 3"),
+            SongAlbumItem(R.drawable.genre, "Album 4")
+        )
+
+        // Setting up RecyclerView with GridLayoutManager
+        val gridLayoutManager = GridLayoutManager(requireContext(), 2) // 2 columns
+        binding.recyclerViewALLAlbum.layoutManager = gridLayoutManager
+
+        // Creating adapter with a lambda to provide the fragment instance
+        adapter = RecyclerViewAlbumAdapter({ this }, listAlbum)
+        binding.recyclerViewALLAlbum.adapter = adapter
 
         return root
-
-
     }
 
     override fun onDestroyView() {
